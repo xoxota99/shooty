88be2d359c9caedca9fb81b0adfc5f21b050242e
Phil <philippe.desrosiers@gmail.com>
2018-02-08 16:43:13 -0800

    Added command-line tests for Servos.

--- Cerberus/servos.py
+++ Cerberus/servos.py
@@ -2,9 +2,9 @@ from __future__ import division
 import math
 import time
 import Adafruit_PCA9685
-# import logging
 
-# logging.basicConfig(level=logging.DEBUG)
+# Set frequency to 50hz, good for servos.
+PWM_FREQ = 50
 
 # Initialise the PCA9685 using the default address (0x40).
 pwm = Adafruit_PCA9685.PCA9685()
@@ -12,16 +12,29 @@ pwm = Adafruit_PCA9685.PCA9685()
 # Alternatively specify a different address and/or bus:
 # pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
 
-# Set frequency to 60hz, good for servos.
-pwm.set_pwm_freq(60)
+pwm.set_pwm_freq(PWM_FREQ)
 
 # Configure min and max servo pulse lengths
-SERVO_MIN = 150  # Min pulse length out of 4096
-SERVO_MAX = 600  # Max pulse length out of 4096
+SERVO_MIN = 150
+SERVO_MAX = 600
 
 # Pin configuration on the Adafruit board. Which pins are driving which servos?
 PAN_PIN = 0
 TILT_PIN = 1
+
+
+def set_servo_pulse(channel, pulse):
+    """
+    Helper function to make setting a servo pulse width simpler.
+    """
+    pulse_length = 1000000    # 1,000,000 us per second
+    pulse_length //= PWM_FREQ       # 60 Hz
+    print('{0}us per period'.format(pulse_length))
+    pulse_length //= 4096     # 12 bits of resolution
+    print('{0}us per bit'.format(pulse_length))
+    pulse *= 1000
+    pulse //= pulse_length
+    pwm.set_pwm(channel, 0, pulse)
 
 
 def move_to(pan, tilt=0):
@@ -39,22 +52,42 @@ def move_to(pan, tilt=0):
     """
     p = math.map(pan, -90, 90, SERVO_MIN, SERVO_MAX)
     t = math.map(tilt, -90, 90, SERVO_MIN, SERVO_MAX)
-    pwm.set_pwm(0, PAN_PIN, p)
-    pwm.set_pwm(0, TILT_PIN, t)
+    set_servo_pulse(PAN_PIN, p)
+    set_servo_pulse(TILT_PIN, t)
 
 
 if __name__ == "__main__":
-    """test the servos."""
+    """
+    Test the servos.
 
-    print('Moving servo on channel 0/1, press Ctrl-C to quit...')
-    while True:
-        # Move servo on channel O between extremes.
-        pwm.set_pwm(0, PAN_PIN, SERVO_MIN)
-        pwm.set_pwm(0, TILT_PIN, SERVO_MIN)
-        time.sleep(1)
-        pwm.set_pwm(0, TILT_PIN, SERVO_MAX)
-        time.sleep(1)
-        pwm.set_pwm(0, PAN_PIN, SERVO_MAX)
-        time.sleep(1)
-        pwm.set_pwm(0, TILT_PIN, SERVO_MIN)
-        time.sleep(1)
+    Usage: servos.py [<pin#> <angle_degrees>]
+    """
+    import sys
+    if(len(sys.argv) == 1):
+        # no arguments.
+        while True:
+            # Move servo on channel O between extremes.
+            set_servo_pulse(PAN_PIN, SERVO_MIN)
+            set_servo_pulse(TILT_PIN, SERVO_MIN)
+            time.sleep(1)
+            set_servo_pulse(TILT_PIN, SERVO_MAX)
+            time.sleep(1)
+            set_servo_pulse(PAN_PIN, SERVO_MAX)
+            time.sleep(1)
+            set_servo_pulse(TILT_PIN, SERVO_MIN)
+            time.sleep(1)
+    elif(len(sys.argv) >= 3):
+        srv_num = int(sys.argv[1])
+        if(srv_num < 16):
+            srv_angle = int(sys.argv[2])
+            if(-90 <= srv_angle <= 90):
+                set_servo_pulse(srv_num, map(srv_angle, -90, 90, SERVO_MIN,
+                                             SERVO_MAX))
+            else:
+                print("Invalid argument <degrees>. Valid values are between -90 \
+                and 90.")
+        else:
+            print("Invalid argument <pin#>. Valid values are between 0 and \
+            15.")
+    else:
+        print("Usage: servos.py [<pin#> <angle_degrees>]")
